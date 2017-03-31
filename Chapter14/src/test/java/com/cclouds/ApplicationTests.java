c9943dfbfd1e72fc7ecdfa0708aead2d41b182fe
dongxiaopeng <dongxiaopeng@zuozh.com>
2017-03-31 13:19:36 +0800

    Spring Boot使用log4j记录日志

---
+++ Chapter14/src/test/java/com/cclouds/ApplicationTests.java
@@ -0,0 +1,29 @@
+package com.cclouds;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Description:Spring Boot使用log4j记录日志
+ *
+ * @author cclouds
+ * @Date Create on 2017/3/30
+ * @since version1.0
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+public class ApplicationTests {
+
+	private Logger logger = Logger.getLogger(getClass());
+
+	@Test
+	public void test() throws Exception {
+		logger.info("输出info");
+		logger.debug("输出debug");
+		logger.error("输出error");
+	}
+
+}
