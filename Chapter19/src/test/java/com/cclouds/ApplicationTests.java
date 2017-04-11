fab29036d0dc872fffe15bed7616f0a46a683f91
dongxiaopeng <dongxiaopeng@zuozh.com>
2017-04-11 14:31:44 +0800

    Spring Boot使用Redis做缓存

--- Chapter19/src/test/java/com/cclouds/ApplicationTests.java
+++ Chapter19/src/test/java/com/cclouds/ApplicationTests.java
@@ -41,7 +41,7 @@ public class ApplicationTests {
         User u2 = userRepository.findByName("AAA");
         System.out.println("第二次查询：" + u2.getAge());
 
-        u1.setAge(20);
+        u1.setAge(30);
         userRepository.save(u1);
         User u3 = userRepository.findByName("AAA");
         System.out.println("第三次查询：" + u3.getAge());
